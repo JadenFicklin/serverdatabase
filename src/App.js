50dc29d1935f6ff4c18a5c8e6dfec7019a9ea99e
JadenFicklin <jadendficklin@gmail.com>
2022-03-11 14:22:10 -0700

    test

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          My name is Jaden
+          Learn React
         </a>
       </header>
     </div>
